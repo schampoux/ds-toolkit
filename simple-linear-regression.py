cbdd606de21fcc05cd213aedb3147ea3ef785142
schampoux <sergiochampoux@gmail.com>
2025-04-21 14:17:21 -0400

    Now we instantiate Prep and SLR and create a model usinng their respective functions.

--- simple-linear-regression.py
+++ simple-linear-regression.py
@@ -1,11 +1,20 @@
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 from preprocessing import Prep
+from linear_models.slr import SLR
 
 def main():
     df = pd.read_csv('./datasets/babies.csv')
-    preprocess = Prep(df = df)
-    preprocess.remove_nans()
+    prep_instance = Prep(df = df)
+    prep_instance.remove_nans()
+    data = prep_instance.df_no_nans
+
+    slr_instance = SLR(df=data)
+    feature = data.gestation.name 
+    target = data.age.name 
+    x, y = slr_instance.x_y(feature = feature, target=target)
+    slr_model = slr_instance.create_model()
+    print(slr_model)
 
 if __name__ =="__main__":
     main()
