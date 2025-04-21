3a69b3129ba68eb2c0c2ca577ea7e143f6f078a7
schampoux <sergiochampoux@gmail.com>
2025-04-21 13:04:48 -0400

    create constructor of Prep class to store the input df as an instance variable. Create function for removing nans

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
 class Prep():
+    def __init__(self, df):
+        self.df = df
+
     def encode_categoricals(df: pd.DataFrame ):
         pass 
+
     def scale_features():
         pass
+
+    def remove_nans(self) -> pd.DataFrame:
+        self.df["row_contains_nan"] = self.df.isnull().any(axis=1)
+        df_no_nans = self.df[~self.df["row_contains_nan"]]
+        output = df_no_nans.drop(columns = "row_contains_nan")
+        return output, print("nans removed")
