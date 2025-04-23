dce2b26d01fba7e1cd3b15cb36270fa4acc40f0e
schampoux <sergiochampoux@gmail.com>
2025-04-22 20:22:48 -0400

    rename slr to ols

--- linear_models/ols.py
+++ linear_models/ols.py
@@ -1,18 +1,21 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd 
 import numpy as np 
+from typing import DefaultDict
 
-class SLR():
+class OLS():
     
     def __init__(self, df: pd.DataFrame):
         self.df = df 
+        self.info_dict = DefaultDict()
 
     def x_y(self, feature: str, target: str):
         """
-        Defines the features matrix and target array to create the model for simple linear regression 
+        Defines the features matrix and target array for SLR model instantiation./ 
         """
-        x = np.array(self.df[feature])
         
+        x = np.array(self.df[feature])
+        self.info_dict["feature"]["name"]
         self.features_matrix = x[:,np.newaxis]
         self.target_array = np.array(self.df[target])
 
@@ -20,6 +23,7 @@ class SLR():
         return self.features_matrix, self.target_array
 
     def create_model(self):
-        slr_model = LinearRegression()
-        self.slr_model = slr_model.fit(self.features_matrix, self.target_array)
-        return self.slr_model
+        ols_inst = LinearRegression()
+        self.ols_fit = ols_inst.fit(self.features_matrix, self.target_array)
+        return self.ols_fit
+    
