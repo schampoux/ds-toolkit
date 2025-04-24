0392b043f133e0d3326fa612c97117615df0c9da
schampoux <sergiochampoux@gmail.com>
2025-04-24 16:58:24 -0400

    remove class structure complexity

--- simple-linear-regression.py
+++ simple-linear-regression.py
@@ -1,20 +1,21 @@
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
-from utilities.preprocessing import Prep
+from utilities.preprocessing import remove_nans, eda_nans
 from linear_models.ols import OLS
+
+
+
 
 def main():
     df = pd.read_csv('./datasets/babies.csv')
-    prep_instance = Prep(df = df)
-    prep_instance.remove_nans()
-    data = prep_instance.df_no_nans
-
-    ols_instance = OLS(df=data)
-    feature = data.gestation.name 
-    target = data.age.name 
-    ols_instance.x_y(feature = feature, target=target)
-    ols_model = ols_instance.create_model()
-    print("OLS model created with the following default attributes: ", "\nrank_", "\ncoef_", "\nsingular_", "\nintercept","\nn_features_in_")
+    eda_nans(df)
+    df = remove_nans(df)
+    # ols_instance = OLS(df=data)
+    # feature = data.gestation.name 
+    # target = data.age.name 
+    # ols_instance.x_y(feature = feature, target=target)
+    # ols_model = ols_instance.create_model()
+    # print("OLS model created with the following default attributes: ", "\nrank_", "\ncoef_", "\nsingular_", "\nintercept","\nn_features_in_")
 
 if __name__ =="__main__":
     main()
--- utilities/preprocessing.py
+++ utilities/preprocessing.py
@@ -1,25 +1,15 @@
 import pandas as pd
 
-class Prep():
+def remove_nans(df) -> pd.DataFrame:
+    df["row_contains_nan"] = df.isnull().any(axis=1)
+    df_no_nans = df[~df["row_contains_nan"]]
+    output = df_no_nans.drop(columns = "row_contains_nan")
+    df_no_nans = output
+    print(f"\nRows with NaN's removed\n")
+    return df_no_nans
 
-    def __init__(self, df):
-        self.df = df
-
-    def remove_nans(self) -> pd.DataFrame:
-        self.df["row_contains_nan"] = self.df.isnull().any(axis=1)
-        df_no_nans = self.df[~self.df["row_contains_nan"]]
-        output = df_no_nans.drop(columns = "row_contains_nan")
-        self.df_no_nans = output
-        return self.df_no_nans, print(f"\nNaN's removed from data via {Prep.remove_nans.__name__} method.\n")
-    
-    def eda_nans(self):
-        print("Number of NaN observations in each column.\n")
-        for i in self.df.columns:
-            print(i, sum(self.df[i].isnull()))
-        print("\n")
-
-    def encode_categoricals(df: pd.DataFrame ):
-        pass 
-
-    def scale_features():
-        pass
+def eda_nans(df):
+    print("Number of NaN observations in each column.\n")
+    for i in df.columns:
+        print(i, sum(df[i].isnull()))
+    print("\n")
