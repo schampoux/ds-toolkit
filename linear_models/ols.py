c6cc3286c98423bb8b05d6cbd0d75cddbc6e9216
schampoux <sergiochampoux@gmail.com>
2025-04-24 17:15:09 -0400

    remove class structure

--- linear_models/ols.py
+++ linear_models/ols.py
@@ -2,28 +2,24 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd 
 import numpy as np 
 from typing import DefaultDict
-
-class OLS():
     
-    def __init__(self, df: pd.DataFrame):
-        self.df = df 
-        self.info_dict = DefaultDict()
+def x_y(df, feature_name: str, target_name: str)-> tuple[pd.DataFrame, pd.Series]:
+    """
+    Defines the features matrix and target array for OLS model instantiation.
+    """
+    
+    x = np.array(df[feature_name])
+    add_axis = x[:,np.newaxis]
+    
+    features_matrix = pd.DataFrame(add_axis, columns = target_name)
+    target_array = pd.Series(df[target_name])
 
-    def x_y(self, feature: str, target: str):
-        """
-        Defines the features matrix and target array for SLR model instantiation./ 
-        """
-        
-        x = np.array(self.df[feature])
-        self.info_dict["feature"]["name"]
-        self.features_matrix = x[:,np.newaxis]
-        self.target_array = np.array(self.df[target])
+    print(f"Created features matrix with shape {features_matrix.shape} and target array with shape {target_array.shape}.")
 
-        print(f"Created features matrix with shape {self.features_matrix.shape} and target array with shape {self.target_array.shape}.")
-        return self.features_matrix, self.target_array
+    return features_matrix, target_array
 
-    def create_model(self):
-        ols_inst = LinearRegression()
-        self.ols_fit = ols_inst.fit(self.features_matrix, self.target_array)
-        return self.ols_fit
+def create_model(features_matrix, target_array):
+    ols_inst = LinearRegression()
+    ols_fit = ols_inst.fit(features_matrix, target_array)
+    return ols_fit
     
