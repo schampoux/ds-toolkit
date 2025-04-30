416c8229531dd85f4298615188eb395897dd3e5e
schampoux <sergiochampoux@gmail.com>
2025-04-30 15:47:38 -0400

    remove unused notebooks

--- ingestion/notebooks/Untitled.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def send_http_request():
-    response = requests.get('https://api.github.com/user')
-    return print(response.status_code)
-
-if __name__ == "__main__":
-    send_http_request()
