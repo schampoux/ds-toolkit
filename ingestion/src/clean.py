c8c730b45dfe7877ed663801bba02263784873c7
schampoux <sergiochampoux@gmail.com>
2025-04-30 10:09:23 -0400

    start creating funcitons to parse xml data

--- ingestion/src/clean.py
+++ ingestion/src/clean.py
@@ -1,9 +1,18 @@
 import os 
-import xml.etree.ElementTree as ET 
+from lxml import etree 
 import json 
 
-def parse_drug_label():
-    pass
+def parse_drug_label(path: str):
+    with open(path) as p:
+        p
+
+    parser = etree.XMLParser(recover=True) # recover = True to ignore errors from parsing invalid characters 
+    root = etree.parse(data = p, parser = parser)
+
+    # tree = et.parse(source = path, parser = parser)
+    # root.tag
+    # et.ElementTree(file=path)
+    return print(root.tag)
 
 def get_section_text():
     pass
