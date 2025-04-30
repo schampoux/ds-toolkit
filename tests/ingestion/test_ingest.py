f98c2b38aa985f27ec161505a1101cde9c4c1fed
schampoux <sergiochampoux@gmail.com>
2025-04-30 10:09:37 -0400

    initiate tests

---
+++ tests/ingestion/test_ingest.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from ingestion.src.ingest import fetch_spl_list, download_spl_xml
+
+class TestIngestion(TestCase):
+    def test_fetch_spl_list(self):
+        pass
+    
