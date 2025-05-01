93659e7c0f0d4439b3a2a61442f1b9e2185b8297
schampoux <sergiochampoux@gmail.com>
2025-05-01 15:05:43 -0400

    restructure

--- utilities/misc.py
+++
@@ -1,9 +0,0 @@
-from lxml import html, etree
-
-def prettyprint_html(element, **kwargs):
-    html_str = html.tostring(doc = element, pretty_print=True, **kwargs)
-    print(html_str.decode(), end='')
-
-def prettyprint_xml(element, **kwargs):
-    xml = etree.tostring(element, pretty_print=True, **kwargs)
-    print(xml.decode(), end='')
