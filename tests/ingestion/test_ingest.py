22a5025d0fced5cd99c0ce7f17ee600143bd608e
schampoux <sergiochampoux@gmail.com>
2025-05-01 17:34:42 -0400

    test for fetch_spl_lists

--- tests/ingestion/test_ingest.py
+++ tests/ingestion/test_ingest.py
@@ -1,8 +1,26 @@
 import unittest
 from toolkit.ingestion.ingest import fetch_spl_list, download_spl_html
+import responses 
 
 class TestIngestion(unittest.TestCase):
+    
+    @responses.activate
     def test_fetch_spl_list(self):
-        pass
+        responses.add(**{
+            'method': responses.GET,
+            'url': 'http://example.com/api/123',
+            'body': '{"data": "content"}',
+            'status': 200,
+            'content_type': 'application/json',
+            'adding_headers': {'X-Foo': 'Bar'}
+        })  
+
+        list = fetch_spl_list(
+            base_url='http://example.com/api/123',
+            limit=1
+            )
+        
+        self.assertEqual("content", list)
+        
     def test_download_spl_html(self):
         pass
