a50d483ca5d7870a057c2f521836f47549214e5e
schampoux <sergiochampoux@gmail.com>
2025-05-01 12:59:35 -0400

    docstring, remove comments, save as json

--- ingestion/src/clean.py
+++ ingestion/src/clean.py
@@ -2,34 +2,35 @@ import os
 import json
 from lxml import html 
 
-def parse_drug_label(path: str):
-    with open(file = path, mode = 'rb') as file:
+def parse_drug_label(input_path: str, output_path: str):
+    """
+    Parse raw .html input and save to /data/processed directory as .json
+    """
+    with open(file = input_path, mode = 'rb') as file:
             file_content = file.read()
+            
     tree = html.fromstring(html = file_content)
     result = {}
 
-    sections = tree.xpath("//h2") # .xpath() performs a global XPath query against the document 
+    sections = tree.xpath("//h2") 
 
     for heading in sections:
         title = heading.text_content().strip().upper()
         content = []
 
-        # gather sibling elements up until the next <h2> or end of section
         next_node = heading.getnext()
         while next_node is not None and next_node.tag != "h2":
-            # append the next node to our content list 
             content.append(next_node.text_content().strip())
-            next_node = next_node.getnext() # move to the node after 
+            next_node = next_node.getnext() 
 
         if content:
-            # if our content list is not empty, add title: content 
-            # for handling content paragraphs, two newlines are inserted in between each. 
             result[title] = "\n\n".join(content)
-    return result 
 
+    with open(output_path, "w") as f:
+         json.dump(result, f)
 
-def get_section_text():
-    pass
+    return 
+
 
 def process_all_tables():
     pass
