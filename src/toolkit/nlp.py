6a23ed7b06060695728186a18d81679d7bfbe00c
schampoux <sergiochampoux@gmail.com>
2025-05-06 20:04:01 -0400

    typing

--- src/toolkit/nlp.py
+++ src/toolkit/nlp.py
@@ -6,11 +6,13 @@ from typing import Dict
 
 nlp = spacy.load("en_core_web_sm")
 
-def extract_entities_from_section(text: str) -> Dict[str, list]:
+def extract_entities_from_section(text: str) -> Dict[str, list[dict]]:
     # Language object 
     nlp = spacy.load("en_core_web_sm")
 
     # Calling the nlp object on a string of text will return a processed Doc object 
     doc = nlp(text)
-    entities = [{"text": ent.text, "label": ent.label} for ent in doc.ents]
+    entities = list(
+        {"text": ent.text, "label": ent.label_} for ent in doc.ents
+        )
     return {'entities': entities}
