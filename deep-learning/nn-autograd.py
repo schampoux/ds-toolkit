32896feea14688419770443d6909a57ff6364ca0
schampoux <sergiochampoux@gmail.com>
2025-06-16 16:09:32 -0400

    function to shuffle indices, training loop for validation set observable

--- deep-learning/nn-autograd.py
+++ deep-learning/nn-autograd.py
@@ -55,12 +55,50 @@ def training_loop_sgd(n_epochs, optimizer, params, t_u, t_c):
 
     return params
 
+def training_loop_v(n_epochs, optimizer, params, train_t_u, train_t_c, val_t_u, val_t_c,):
+    for epoch in range(1, n_epochs+1):
+        train_t_p = model(train_t_u, *params)
+        train_loss = loss_fn(train_t_p, train_t_c)
+
+        # Compute validation set loss for monitoring during training
+        val_t_p = model(val_t_u, *params)
+        val_loss = loss_fn(val_t_p, val_t_c)
+
+        optimizer.zero_grad()
+        train_loss.backward()
+        optimizer.step()
+
+        if epoch <=3 or epoch % 500 == 0:
+            print(f'Epoch {epoch}, Training Loss {round(float(train_loss),4)}, \n\tValidation Loss {round(float(val_loss),4)}')
+
+    return params
     
+def shuffle_indices(t_u, t_c)-> tuple[torch.tensor]:
+    n_samples = t_u.shape[0]
+    n_val = int(0.2 * n_samples) 
+
+    shuffled_indices = torch.randperm(n_samples)
+    train_indices = torch.randperm(n_samples) 
+
+    train_indices = shuffled_indices[:-n_val]
+    val_indices = shuffled_indices[-n_val:]
+
+    train_t_u = t_u[train_indices]
+    train_t_c = t_c[train_indices]
+
+    val_t_u = t_u[val_indices]
+    val_t_c = t_c[val_indices]
+
+    return train_t_u, train_t_c, val_t_u, val_t_c
+
 def main(): 
     t_c = torch.tensor([0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0])
     t_u = torch.tensor([35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4])
+
+    # Normalize
     t_un = t_u*.1
 
+    
     # params = training_loop(
     #     n_epochs = 5000,
     #     learning_rate = 1e-2,
@@ -69,16 +107,39 @@ def main():
     #     t_c = t_c
     # )
 
-    # SGD Optimizer 
+
+    # # SGD Optimizer
+
+    # params = torch.tensor([1.0,0.0], requires_grad=True)
+    # learning_rate = 1e-2
+    # optimizer = torch.optim.SGD([params], lr=learning_rate)
+    # trained_params = training_loop_sgd(
+    #     n_epochs = 5000,
+    #     optimizer = optimizer,
+    #     params = params,
+    #     t_u = t_un,
+    #     t_c = t_c
+    # )
+    # print(trained_params)
+
+    # Validation set 
     params = torch.tensor([1.0,0.0], requires_grad=True)
     learning_rate = 1e-2
     optimizer = torch.optim.SGD([params], lr=learning_rate)
-    trained_params = training_loop_sgd(
+    train_t_u, train_t_c, val_t_u, val_t_c = shuffle_indices(t_u, t_c)
+
+    # Normalize
+    train_t_un = 0.1 * train_t_u
+    val_t_un = 0.1 * val_t_u
+
+    trained_params = training_loop_v(
         n_epochs = 5000,
         optimizer = optimizer,
         params = params,
-        t_u = t_un,
-        t_c = t_c
+        train_t_u = train_t_un,
+        train_t_c = train_t_c, 
+        val_t_u = val_t_un, 
+        val_t_c = val_t_c,
     )
     print(trained_params)
 
