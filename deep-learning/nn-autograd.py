a37a4774684122c7947218a8c06dea3794dd5767
schampoux <sergiochampoux@gmail.com>
2025-06-16 15:04:14 -0400

    support SGD optimizer

--- deep-learning/nn-autograd.py
+++ deep-learning/nn-autograd.py
@@ -1,4 +1,5 @@
-import torch 
+import torch
+import torch.optim
 import logging 
 
 def model(t_u, w, b): 
@@ -32,27 +33,56 @@ def training_loop(n_epochs, learning_rate, params, t_u, t_c):
         with torch.no_grad():
             params -= learning_rate * params.grad
 
+        # Log
         if epoch % 500 == 0: 
             print(f'Epoch {epoch}, Loss {float(loss)}')
+
     return params
 
+def training_loop_sgd(n_epochs, optimizer, params, t_u, t_c):
+    for epoch in range(1, n_epochs+1):
+        t_p = model(t_u, *params)
+        loss = loss_fn(t_p, t_c)
+        
+        # Zero the gradients
+        optimizer.zero_grad()
+        loss.backward()
+
+        # Update params
+        optimizer.step()
+
+        # Log
+        if epoch % 500 == 0: 
+            print(f'Epoch {epoch}, Loss {float(loss)}')
+
+    return params
+
+    
 def main(): 
     t_c = torch.tensor([0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0])
     t_u = torch.tensor([35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4])
     t_un = t_u*.1
-    # Track the entire family tree of tensors resulting from operations on params: `requires_grad=True`
-        # Any tensor that has params as an ancestor will have access to the chain of functions that were called on it.
-            # In the case that the functions are differentiable, 
-            # the value of the derivative will be automatically populated as a grad attr. of the params tensor
-    params = training_loop(
+
+    # params = training_loop(
+    #     n_epochs = 5000,
+    #     learning_rate = 1e-2,
+    #     params = torch.tensor([1.0,0.0], requires_grad=True),
+    #     t_u = t_un,
+    #     t_c = t_c
+    # )
+
+    # SGD Optimizer 
+    params = torch.tensor([1.0,0.0], requires_grad=True)
+    learning_rate = 1e-2
+    optimizer = torch.optim.SGD([params], lr=learning_rate)
+    trained_params = training_loop_sgd(
         n_epochs = 5000,
-        learning_rate = 1e-2,
-        params = torch.tensor([1.0,0.0], requires_grad=True),
+        optimizer = optimizer,
+        params = params,
         t_u = t_un,
         t_c = t_c
     )
-    return print(params.grad) 
-
+    print(trained_params)
 
 if __name__ =="__main__":
     main() 
